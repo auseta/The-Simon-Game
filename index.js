7c28fa33f210ca063cd3c60b3f6e77a606ba828b
auseta <alseta@gmail.com>
2023-10-09 21:56:48 -0300

    updated index.js
    finished the logic that allows you to play the game.

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
 let gamePattern = [];
+let userClickedPattern = [];
+
 const buttonColours = ["red", "blue", "green", "yellow"];
+
+let started = false;
+let level = 0;
 
 $(".btn").on("click", function(){
     let userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour, 100);
+
+    checkAnswer(userClickedPattern.length - 1);
+
 })
 
+$(document).on("keydown", function(){
+    if(!started) {
+        started = true;
+        nextSequence();
+    }
+})
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        console.log("wrong")
+    }
+}
+
 function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+
+    $("h1").text("Level " + level);
+
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
 
     gamePattern.push(randomChosenColour);
 
-    lightsAnimation(randomChosenColour);
+    animatePress(randomChosenColour, 1000);
 
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 
 }
 
-function lightsAnimation(colour) {
-    $("#" + colour).css("box-shadow","0px 0px 15px 2px " + colour);
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(colour, time) {
+    $("#" + colour).css("box-shadow","0px 0px 15px 2px " + colour).addClass("pressed");
     setTimeout(() => {
-        $("#" + colour).css("box-shadow", "none");
-    }, 1000);
+        $("#" + colour).css("box-shadow", "none").removeClass("pressed");
+    }, time);
 }
